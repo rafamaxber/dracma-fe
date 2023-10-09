e6d2fa1c02b3bc1c94cdccb43f3d3ef64da45311
Rafael Bernardo <rafamaxber@gmail.com>
2023-10-09 01:52:21 -0300

    '[app/bank/page.tsx] Updated BankForm component with new fields and validations'
    '

--- src/app/bank/page.tsx
+++ src/app/bank/page.tsx
@@ -1,27 +1,69 @@
-export default function Bank() {
-  return (
-    <main>
-      <h1>Saldos iniciais</h1>
+"use client"
 
-      <div className="">
-        <label htmlFor="bank">Data do saldo inicial</label>
-        <input className="text-black" type="date" name="bank" id="bank" />
-      </div>
 
-      <div className="">
-        <h2>Informe o banco e saldo total</h2>
-
-        <form className="flex gap-8">
-          <div>
-            <label htmlFor="bank_name">Banco:</label>
-            <input className="text-black" type="text" name="bank_name" id="bank_name" />
-          </div>
-          <div>
-            <label htmlFor="balance">Saldo:</label>
-            <input className="text-black" type="text" name="balance" id="balance" />
-          </div>
-        </form>
-      </div>
-    </main>
-  )
+export default function BankForm() {
+return (
+  <div>teste</div>
+)
+  // return (
+  //   <form onSubmit={handleSubmit(onSubmit)} className="shadow-md rounded px-8 pt-6 pb-8 mb-4">
+  //     <div className="mb-4">
+  //       <label className="block text-gray-700 font-bold mb-2" htmlFor="bank">
+  //         Bank
+  //       </label>
+  //       <input
+  //         className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+  //           errors?.bank ? 'border-red-500' : ''
+  //         }`}
+  //         id="bank"
+  //         type="text"
+  //         placeholder="Enter bank name"
+  //         {...register('bank', { required: true, minLength: 3 })}
+  //       />
+  //       {errors?.bank && <p className="text-red-500 text-xs italic">Bank name is required</p>}
+  //     </div>
+  //     <div className="mb-4">
+  //       <label className="block text-gray-700 font-bold mb-2" htmlFor="balance">
+  //         Balance
+  //       </label>
+  //       <input
+  //         className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+  //           errors?.balance ? 'border-red-500' : ''
+  //         }`}
+  //         id="balance"
+  //         type="text"
+  //         placeholder="Enter balance"
+  //         {...register('balance', { required: true, min: 0, valueAsNumber: true })}
+  //       />
+  //       {errors?.balance && (
+  //         <p className="text-red-500 text-xs italic">Balance must be greater than or equal to 0</p>
+  //       )}
+  //     </div>
+  //     <div className="mb-4">
+  //       <label className="block text-gray-700 font-bold mb-2" htmlFor="initialDate">
+  //         Initial Date
+  //       </label>
+  //       <input
+  //         className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+  //           errors?.initialDate ? 'border-red-500' : ''
+  //         }`}
+  //         id="initialDate"
+  //         type="date"
+  //         placeholder="Enter initial date"
+  //         {...register('initialDate', { required: true })}
+  //       />
+  //       {errors?.initialDate && (
+  //         <p className="text-red-500 text-xs italic">Initial date is required</p>
+  //       )}
+  //     </div>
+  //     <div className="flex items-center justify-between">
+  //       <button
+  //         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+  //         type="submit"
+  //       >
+  //         Add Bank
+  //       </button>
+  //     </div>
+  //   </form>
+  // );
 }
